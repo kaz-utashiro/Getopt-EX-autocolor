77bdf7172c1a422a55d063fce311000c34577021
Kazumasa Utashiro <kaz@utashiro.com>
2020-04-10 11:50:14 +0900

    minil new Getopt::EX::autocolor

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Getopt::EX::autocolor - It's new $module
+
+# SYNOPSIS
+
+    use Getopt::EX::autocolor;
+
+# DESCRIPTION
+
+Getopt::EX::autocolor is ...
+
+# LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazumasa Utashiro <kaz@utashiro.com>
---
+++ lib/Getopt/EX/autocolor.pm
@@ -0,0 +1,39 @@
+package Getopt::EX::autocolor;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Getopt::EX::autocolor - It's new $module
+
+=head1 SYNOPSIS
+
+    use Getopt::EX::autocolor;
+
+=head1 DESCRIPTION
+
+Getopt::EX::autocolor is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
